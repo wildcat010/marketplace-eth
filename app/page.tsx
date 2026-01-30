7ad11e3e369f396ef9b6192411a76333fcccf190
Benoit Bourgeon <benoit.bourgeon.ext@primetals.com>
2026-01-30 15:43:05 +0100

    #109

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,53 @@
-import Image from "next/image";
+import Navbar from "@components/common/navbar";
+import Footer from "@components/common/footer";
+
+import Hero from "@components/common/hero";
+import Breacrumps from "@components/common/breadcrumbs";
+
+import Walletbar from "@components/web3/walletbar";
+import EthRates from "@components/web3/ethRates";
+
+import Card from "@components/order/card";
+import List from "@components/order/list";
 
 export default function Home() {
-  return <div>Hello world</div>;
+  return (
+    <div>
+      <div className="relative bg-white overflow-hidden">
+        <div className="relative max-w-7xl mx-auto px-4">
+          {/*------ NAVBAR STARTS ------*/}
+          <Navbar></Navbar>
+          {/*------ NAVBAR ENDS ------*/}
+          <div className="fit">
+            {/*------ HERO STARTS ------*/}
+            <Hero></Hero>
+            {/*------ HERO ENDS ------*/}
+
+            {/*------ BREADCRUMBS STARTS ------*/}
+            <Breacrumps></Breacrumps>
+            {/*------ BREADCRUMBS ENDS ------*/}
+
+            {/*------ ADDRESS STARTS ------*/}
+            <Walletbar></Walletbar>
+            {/*------ ADDRESS ENDS ------*/}
+
+            {/*------ CURRENCY STARTS ------*/}
+            <EthRates></EthRates>
+            {/*------ CURRENCY ENDS ------*/}
+
+            {/*------ ORDER INFO STARTS ------*/}
+            <Card></Card>
+            {/*------ ORDER INFO ENDS ------*/}
+
+            {/*------ COURSE CARD STARTS ------*/}
+            <List></List>
+            {/*------ COURSE CARD ENDS ------*/}
+          </div>
+        </div>
+        {/*------ FOOTER STARTS ------*/}
+        <Footer></Footer>
+        {/*------ FOOTER ENDS ------*/}
+      </div>
+    </div>
+  );
 }
